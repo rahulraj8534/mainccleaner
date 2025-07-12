7428aaf601965118f876fa694bf9d08205d7605a
rahulraj8534 <rahulraj8534@outlook.com>
2025-07-13 00:27:04 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -5,7 +5,7 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 
 # Project information
-project = 'webrootmain'
+project = 'Ccleaner Main'
 author = 'Author'
 
 # -- General configuration ---------------------------------------------------
